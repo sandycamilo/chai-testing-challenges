8b00e8e26abba67975ae3023bee2e8af93a2d8a3
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-03-04 21:02:35 -0800

    tests done

--- src/routes/message.js
+++ src/routes/message.js
@@ -7,16 +7,27 @@ const Message = require('../models/message')
 /** Route to get all messages. */
 router.get('/', (req, res) => {
     // TODO: Get all Message objects using `.find()`
-
+    let message = Message(req.body)
+    message.find()
+    .then(message => {
+        return res.find(message)
+    })
     // TODO: Return the Message objects as a JSON list
+    .then(message => {
+        return req.json(message)
+    })
 })
 
 /** Route to get one message by id. */
 router.get('/:messageId', (req, res) => {
+    let message = Message(req.body)
+    message.findOne(req.params.messageId)
     // TODO: Get the Message object with id matching `req.params.id`
-    // using `findOne`
-
+     // using `findOne`
     // TODO: Return the matching Message object as JSON
+    .then(message => {
+        return res.json(message)
+    })
 })
 
 /** Route to add a new message. */
@@ -41,16 +52,23 @@ router.post('/', (req, res) => {
 /** Route to update an existing message. */
 router.put('/:messageId', (req, res) => {
     // TODO: Update the matching message using `findByIdAndUpdate`
-
-    // TODO: Return the updated Message object as JSON
+    let message = Message(req.body) 
+    message.findByIdAndUpdate(req.params.messageId)
+    .then(message => {
+        return res.json(message)
+    })
 })
 
 /** Route to delete a message. */
 router.delete('/:messageId', (req, res) => {
     // TODO: Delete the specified Message using `findByIdAndDelete`. Make sure
     // to also delete the message from the User object's `messages` array
-
+    let message = Message(req.body)
+    message.findByIdAndDelete(req.parads.messageId)
     // TODO: Return a JSON object indicating that the Message has been deleted
+    .then(message => {
+        return res.json('message has been deleted')
+    })
 })
 
 module.exports = router
--- src/test/message.js
+++ src/test/message.js
@@ -25,40 +25,142 @@ after((done) => {
   done()
 })
 
-
 describe('Message API endpoints', () => {
     beforeEach((done) => {
         // TODO: add any beforeEach code here
+        user_one = new User({username: "M1", password: "mmone"})
+        user_two = new User({username: "M2", password: "mmtwo"})
+        message_one = new Message({
+            title: "Important Message",
+            body: "Hello there",
+            author: user_one
+        })
+        message_two = new Message({
+            title = "Another Important Message",
+            body: "Hi there",
+            author: user_two
+        })
+        users.push(user_one)
+        users.push(user_two)
+        messages.push(message_one)
+        messages.push(message_two)
+        
+        users.forEach(user => {
+            user.save()
+        })
+        messages.forEach(message => {
+            message.save()
+        })
         done()
     })
 
     afterEach((done) => {
         // TODO: add any afterEach code here
+        users.forEach(user => {
+            User.findByIdAndDelete(user.id)
+        })
+        messages.forEach(message => {
+            Message.findByIdAndDelete(message.id)
+        })
         done()
     })
 
     it('should load all messages', (done) => {
         // TODO: Complete this
+        chai
+        .request(app)
+        .get("/messages")
+        .end(function(err, res) {
+            if (err) {
+                return done(err)
+            }
+            res.status.should.equal(200)
+            res.body.should.be.a("array")
+            res.body.should.have.lengthOf.above(3)
+        })
         done()
     })
 
     it('should get one specific message', (done) => {
         // TODO: Complete this
+        chai
+        .request(app)
+        .get("/messages/" + message_one.id)
+        .end(function(err, res) {
+            if (err) {
+                return done(err)
+            }
+            res.status.should.equal(200)
+            res.body.should.be.a("Object")
+            res.body.shoud.have.a.property("title").and.to.equal(message_one.title)
+            res.body.shoud.have.a.property("body").and.to.equal(message_one.body)
+            res.body.shoud.have.a.property("author").and.to.equal(message_one.author)
+        })
         done()
     })
 
     it('should post a new message', (done) => {
         // TODO: Complete this
+        let newPost = {
+            title: "Important Message3",
+            body: "How are you?!",
+            author: "user_one.id"
+        }
+        chai
+        .request(app)
+        .post("/messages")
+        .type("form")
+        .send(newPost)
+        .end(function(err, res) {
+            if (err) {
+                return done(err)
+            }
+            Message.findById(res.body.id)
+                .catch(err => {
+                    return done(err)
+                })
+            res.status.shoud.equal(200)
+            res.body.should.be.a("Object")
+            res.body.should.have.property("title").and.to.equal(newPost.title)
+            res.body.should.have.property("body").and.to.equal(newPost.body)
+            res.body.should.have.property("author").and.be.equal(newPost.author)
+        })
         done()
     })
 
     it('should update a message', (done) => {
         // TODO: Complete this
+        chai
+        .request(app)
+        .put("/messages/" + messages[0].id)
+        .type("form")
+        .send({
+            title: "Update Message",
+            body: messages[0].body,
+            author: messages[0].author.id
+        })
+        .end(function(err, res) {
+            if (err) {
+                return done(err)
+            }
+            Message.findById(res.body.id)
+                .catch(err => {
+                    return done(err)
+                })
+            res.status.should.equal(200)
+            res.body.should.have.property("title").and.to.equal("Update Message")
+            res.body.should.have.property("body").and.to.equal(messages[0].body)
+            res.body.should.have.property("author").and.be.equal(messages[0].author)
+        })
         done()
     })
 
     it('should delete a message', (done) => {
         // TODO: Complete this
+        let id = messages[1].id
+        chai
+        .request(app)
+        .delete("/messages/" + id)
         done()
     })
 })
